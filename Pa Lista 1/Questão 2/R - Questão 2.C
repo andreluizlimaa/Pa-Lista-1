5f32ad220d8350c027b3e50db503e10120cb1df3
André luiz <Andreluizlimaa@gmail.com>
2022-03-28 17:43:35 -0300

    Segunda questão

--- Pa Lista 1/Questão 2/R - Questão 2.C
+++ Pa Lista 1/Questão 2/R - Questão 2.C
@@ -9,7 +9,7 @@ int main(void)
  return 0;
  // será impresso primeramente o endereço de p.
  // sendo a expressão *p+2, sabendo que *p vale 5, temos 5+2=7.
- // **&p será terá somente o valor 5.
+ // **&p obtemos somente o valor 5.
  // 3**p, resulta em 3*5=15.
  // **&p+4, terá o resultado 5+4=9.
 
