15c1f9436f3e665b44ab601853d6437ee4e1daa6
André luiz <Andreluizlimaa@gmail.com>
2022-03-28 17:32:05 -0300

    Segunda questão de ponteiros

--- Pa Lista 1/Questão 2/R - Questão 2.C
+++ Pa Lista 1/Questão 2/R - Questão 2.C
@@ -2,14 +2,15 @@
 
 int main(void)
 {
-    int i = 3, j = 5;
-    int *p, *q;
-    p = &i;
-    q = &j;
 
-    // Determinando o valor das seguintes expressões:
-    printf("p == &i = %d;\n", p == &i);
-    printf("*p - *q = %d;\n", p - *q);
-    printf("*&p= %d;\n", *&p);
-    printf("3 - *p / (*q) + 7= %d;", 3 - *p / (*q) + 7);
+ int i=5, *p;
+ p= &i;
+ printf("%x %d %d %d %d",p,*p+2,**&p,3**p,**&p+4);
+ return 0;
+ // será impresso primeramente o endreço de p.
+ // sendo a expressão *p+2, sabendo que *p vale 5, temos 5+2=7.
+ // **&p será terá somente o valor 5.
+ // 3**p, resulta em 3*5=15.
+ // **&p+4, terá o resultado 5+4=9.
+
 }                                                                           
