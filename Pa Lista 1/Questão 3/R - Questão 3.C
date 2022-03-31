1608f283952991297b19f2a9be4bdc838dcd520e
André luiz <Andreluizlimaa@gmail.com>
2022-03-31 09:50:16 -0300

    Terceira questão

--- Pa Lista 1/Questão 3/R - Questão 3.C
+++ Pa Lista 1/Questão 3/R - Questão 3.C
@@ -2,13 +2,13 @@
 
  int i,j,*p,*q;
 p = i; //é uma expressão ilegal, por p ser um ponteiro inteiro que guarda endereço, e i é uma variável inteira.
-q = &j; // expressão válida.
-p = &*&i; // expressão válida.
-i = (*&)j; // expressão errada por conta do parenteses, que impede assim de haver interação de *& com o j para funcionar..
-i = *&j; // expressão válida.
+q = &j; // expressão legal.
+p = &*&i; // expressão legal.
+i = (*&)j; // expressão ilegal por conta do parenteses, que impede assim de haver interação de *& com o j para funcionar..
+i = *&j; // expressão legal.
 i = *&*&j; // expressão válida.
-q = *p; // expressão inválida, por conta do q está recebendo o valor que o ponteiro p indica, e não o endereço.
-i = (*p)++ + *q;// expressão válida.
+q = *p; // expressão ilegal, por conta do q está recebendo o valor que o ponteiro p indica, e não o endereço.
+i = (*p)++ + *q;// expressão legal.
 
 
 
