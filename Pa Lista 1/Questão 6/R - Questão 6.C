3a24ff67db6c47032897fa15963692afb712a204
André luiz <Andreluizlimaa@gmail.com>
2022-04-14 09:27:22 -0300

    Sexta questão

---
+++ Pa Lista 1/Questão 6/R - Questão 6.C
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void){
+
+int pulo[4]={1,2,3,4};
+*(pulo+2);//Expressão para o terceiro elemento.
+*(pulo+4);//Expressão para quinto elemento.
+pulo+4;//Expressão para o endereço do quinto elemento.
+pulo+2;//Expressão para o endereço do terceiro elemento.
+}
