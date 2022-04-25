3fa18c6d874728ba71235c64294725c4cb9571de
André luiz <Andreluizlimaa@gmail.com>
2022-04-25 15:02:00 -0300

    Questão 6

--- Pa Lista 1/Questão 6/R - Questão 6.C
+++ Pa Lista 1/Questão 6/R - Questão 6.C
@@ -3,6 +3,7 @@
 int main(void){
 
 int pulo[4]={1,2,3,4};
+
 *(pulo+2);//Expressão resulta no valor do terceiro elemento.
 *(pulo+4);//Expressão tem-se como resultado o valor do quinto elemento.
 pulo+4;//Expressão que tem como resultado endereço o quinto elemento.
